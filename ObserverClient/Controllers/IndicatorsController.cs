30301d2bfabbae6c5b5fc5c2f7ab5e31165c044b
Maria <maria.landc@gmail.com>
2014-09-05 16:40:32 +0300

    first commit

---
+++ ObserverClient/Controllers/IndicatorsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace ObserverClient.Controllers
+{
+    public class IndicatorsController : Controller
+    {
+        // GET: All Indicators
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Temperature
+        public ActionResult Temperature()
+        {
+            return View();
+        } 
+        
+        // GET: Humidity
+        public ActionResult Humidity()
+        {
+            return View();
+        }
+    }
+}
---
+++ ObserverClient/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ObserverClient.Startup))]
+namespace ObserverClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
